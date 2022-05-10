4d27c65596d577222af02ebb62fcaa3f8f176af4
Naitsyrk <wierzbickikrystian@gmail.com>
2022-05-10 20:37:04 +0200

    is_taken added to model Donation

--- GoodHands/InGoodHandsApp/models.py
+++ GoodHands/InGoodHandsApp/models.py
@@ -41,3 +41,4 @@ class Donation(models.Model):
     pick_up_time = models.TimeField()
     pick_up_comment = models.TextField(null=True, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    is_taken = models.BooleanField(default=False)
